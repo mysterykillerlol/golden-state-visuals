8cd66cc7a499970708b26e0a5b8a154df9b64c7d
mysterykillerlol <105387716+mysterykillerlol@users.noreply.github.com>
2026-04-24 14:11:56 -0700

    Update vite.config.ts

--- gsv/vite.config.ts
+++ gsv/vite.config.ts
@@ -1,18 +1,15 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import path from "path";
-
-export default defineConfig({
-  base: "/",
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "src"),
-      "@assets": path.resolve(__dirname, "..", "..", "attached_assets"),
-    },
+{
+  "compilerOptions": {
+    "target": "ESNext",
+    "useDefineForClassFields": true,
+    "lib": ["DOM", "DOM.Iterable", "ESNext"],
+    "module": "ESNext",
+    "skipLibCheck": true,
+    "moduleResolution": "bundler",
+    "resolveJsonModule": true,
+    "isolatedModules": true,
+    "noEmit": true,
+    "jsx": "react-jsx"
   },
-  build: {
-    outDir: "dist",
-    emptyOutDir: true,
-  },
-});
+  "include": ["src"]
+}
